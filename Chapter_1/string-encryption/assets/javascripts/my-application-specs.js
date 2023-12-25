5620e8a14c98d8d0ce79f97585f9c1fbc62d1101
AldoLunaBueno <alunab@uni.pe>
2023-12-25 18:26:38 -0500

    Move project String Encryption to Chpater 1 directory

---
+++ Chapter_1/string-encryption/assets/javascripts/my-application-specs.js
@@ -0,0 +1,5 @@
+describe("Encrypt String", function(){
+  it("returns the next character", function(){
+    expect(encrypt('a')).toEqual('b')
+  })
+})
--- string-encryption/assets/javascripts/my-application-specs.js
+++
@@ -1,5 +0,0 @@
-describe("Dummy Spec List", function(){
-  it("true is equal to true", function(){
-    expect(true).toEqual(true)
-  })
-})
