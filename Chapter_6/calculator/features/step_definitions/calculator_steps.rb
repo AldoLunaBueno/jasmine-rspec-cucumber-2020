03843eee229a3df7cd1ba89204ee27acfa58e764
AldoLunaBueno <alunab@uni.pe>
2023-12-26 00:51:34 -0500

    WIP: Chapter 6. Cucumber - install cucumber - cucumber init - features, scenarios and steps - features and step_definitions files - Gherkin - pending steps - pass runtime args from step invocations to step definitions

---
+++ Chapter_6/calculator/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,14 @@
+#File: features/step_definitions/calculator_steps.rb
+#
+Given("the input {string}") do |string|
+  pending
+end
+
+When("the calculator is run") do
+  pending
+end
+
+Then("the output should be {string}") do |string|
+  pending
+end
+
